1f7537b2036bbc4b39ee47a7d30937f52b9c3222
Omotosho Yakub Adegbola <105563091+Omotoshowhykay@users.noreply.github.com>
2022-06-22 14:23:26 +0100

    Add files via upload
    JavaScript Assignment No2

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// JavaScript function that print out my name, country and height. 
+
+
+function myInfo() {
+    var name ="Yakub";
+    var country ="Nigeria";
+    var height ="6ft.2in";
+    alert(name + " is from " + country + " and he's " + height + " tall ");
+}
+myInfo();
